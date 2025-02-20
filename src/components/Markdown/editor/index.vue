398ed3bfe6a546ed4a79263800b4598de44e1d7a
wubo <2551921037@qq.com>
2025-02-20 21:56:17 +0800

    将Markdown编辑器升级到Toast UI并添加实用程序过滤器

--- src/components/Markdown/editor/index.vue
+++ src/components/Markdown/editor/index.vue
@@ -1,16 +1,16 @@
 <template>
-  <div :id="id" />
+  <div class="markdown-editor">
+    <div :id="id"></div>
+  </div>
 </template>
 
 <script>
-import 'tui-editor/dist/tui-editor.css'
-import 'tui-editor/dist/tui-editor-contents.css'
-import Editor from 'tui-editor'
-import defaultOptions from './default-options'
-import { uploadFile } from '@/api/upload' // 引入上传文件的接口
+import '@toast-ui/editor/dist/toastui-editor.css'
+import { Editor } from '@toast-ui/editor'
+import { uploadFile } from '@/api/upload'
 
 export default {
-  name: 'WYSIWYGEditor',
+  name: 'MarkdownEditor',
   props: {
     value: {
       type: String,
@@ -18,132 +18,110 @@ export default {
     },
     id: {
       type: String,
-      required: false,
       default() {
-        return 'wysiwyg-editor-' + +new Date() + ((Math.random() * 1000).toFixed(0) + '')
-      }
-    },
-    options: {
-      type: Object,
-      default() {
-        return defaultOptions
+        return 'markdown-editor-' + Date.now()
       }
     },
     height: {
       type: String,
-      required: false,
-      default: '300px'
+      default: '400px'
     },
-    language: {
+    placeholder: {
       type: String,
-      required: false,
-      default: 'zh_CN'
+      default: '请输入内容...'
     }
   },
   data() {
     return {
-      editor: null
-    }
-  },
-  computed: {
-    editorOptions() {
-      const options = Object.assign({}, defaultOptions, this.options)
-      options.initialEditType = 'wysiwyg'  // Always force WYSIWYG mode
-      options.height = this.height
-      options.language = this.language
-
-      // 添加图片上传配置
-      options.events = {
-        addImageBlobHook: this.handleImageUpload
+      editor: null,
+      editorOptions: {
+        el: null,
+        height: this.height,
+        initialEditType: 'markdown',
+        previewStyle: 'vertical',
+        placeholder: this.placeholder,
+        language: 'zh-CN',
+        toolbarItems: [
+          ['heading', 'bold', 'italic', 'strike'],
+          ['hr', 'quote'],
+          ['ul', 'ol', 'task'],
+          ['table', 'image', 'link'],
+          ['code', 'codeblock'],
+          ['scrollSync']
+        ],
+        hooks: {
+          addImageBlobHook: this.handleImageUpload
+        }
       }
-
-      return options
     }
   },
   watch: {
-    value(newValue, preValue) {
-      if (newValue !== preValue && this.editor && newValue !== this.editor.getValue()) {
-        this.editor.setValue(newValue)
+    value(newVal) {
+      if (newVal !== this.editor.getMarkdown()) {
+        this.editor.setMarkdown(newVal)
       }
     },
-    language(val) {
-      if (this.editor) {
-        this.destroyEditor()
-        this.initEditor()
-      }
-    },
-    height(newValue) {
-      if (this.editor) {
-        this.editor.height(newValue)
-      }
+    height(newVal) {
+      this.editor.setHeight(newVal)
     }
   },
   mounted() {
     this.initEditor()
   },
   beforeDestroy() {
-    this.destroyEditor()
+    if (this.editor) {
+      this.editor.destroy()
+    }
   },
   methods: {
     initEditor() {
-      this.editor = new Editor({
-        el: document.getElementById(this.id),
-        ...this.editorOptions
-      })
+      this.editorOptions.el = document.getElementById(this.id)
+      this.editor = new Editor(this.editorOptions)
 
+      // 设置初始内容
       if (this.value) {
-        this.editor.setValue(this.value)
+        this.editor.setMarkdown(this.value)
       }
 
+      // 监听内容变化
       this.editor.on('change', () => {
-        this.$emit('input', this.editor.getValue())
+        const content = this.editor.getMarkdown()
+        this.$emit('input', content)
+        this.$emit('change', content)
       })
     },
-    destroyEditor() {
-      if (!this.editor) return
-      this.editor.off('change')
-      this.editor.remove()
-    },
-    setValue(value) {
-      if (this.editor) {
-        this.editor.setValue(value)
-      }
-    },
-    getValue() {
-      if (this.editor) {
-        return this.editor.getValue()
-      }
-      return ''
-    },
-    setHtml(value) {
-      if (this.editor) {
-        this.editor.setHtml(value)
-      }
-    },
-    getHtml() {
-      if (this.editor) {
-        return this.editor.getHtml()
-      }
-      return ''
-    },
-    // 处理图片上传
-    async handleImageUpload(blob, callback) {
+    async handleImageUpload(file) {
       try {
         const formData = new FormData()
-        formData.append('file', blob)
+        formData.append('file', file)
 
-        // 调用上传接口
-        const res = await uploadFile(formData)
-        const imageUrl = res.data // 获取上传后的图片URL
-
-        // 将图片URL插入到Markdown编辑器中
-        callback(imageUrl, '图片描述')
-        this.$message.success('图片上传成功')
+        const response = await uploadFile(formData)
+        if (response.code === 200) {
+          return response.data
+        } else {
+          throw new Error(response.message || '上传失败')
+        }
       } catch (error) {
         console.error('图片上传失败:', error)
-        this.$message.error('图片上传失败')
+        this.$message.error('图片上传失败：' + error.message)
+        return false
       }
     }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.markdown-editor {
+  width: 100%;
+  
+  :deep(.toastui-editor-defaultUI) {
+    border: 1px solid #dcdfe6;
+    border-radius: 4px;
+  }
+  
+  :deep(.toastui-editor-defaultUI-toolbar) {
+    border-bottom: 1px solid #dcdfe6;
+  }
+}
+</style>
